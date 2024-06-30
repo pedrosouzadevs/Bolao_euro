2be36f10158789f76563cdb26b81725cf75ecb1c
Pedro Souza <phmartinsdesouza@gmail.com>
2024-06-30 16:45:04 -0300

    result model was fixed

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,7 +2,7 @@ class Result < ApplicationRecord
   belongs_to :match
   belongs_to :team_a, class_name: 'Team'
   belongs_to :team_b, class_name: 'Team'
-  belongs_to :winner, class_name: 'Team'
+  belongs_to :winner_id, class_name: 'Team'
   after_save :refresh_bets
 
   private
