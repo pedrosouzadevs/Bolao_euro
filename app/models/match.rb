a5a70147f04c1ee9deb7d38176ef56cc8da3bf43
Pedro Souza <phmartinsdesouza@gmail.com>
2024-07-02 15:10:42 -0300

    match model has one thing wrong, soo it was fixed

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,7 @@ class Match < ApplicationRecord
   belongs_to :team_a, class_name: 'Team'
   belongs_to :team_b, class_name: 'Team'
   belongs_to :round
-  validates :team_a, presence: true, uniqueness: true
-  validates :team_b, presence: true, uniqueness: true
+  validates :team_a, presence: true, uniqueness: { scope: :round_id }
+  validates :team_b, presence: true, uniqueness: { scope: :round_id }
   validates :round_id, presence: true
 end
